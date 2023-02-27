86d9214f5941a317507fe7b9e6d954c9319d613b
Sweeny Mbuvi <sweenymbuvi@gmail.com>
2023-02-27 20:36:27 +0300

    i put my name

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         StartUp startUp = new StartUp();
+        System.out.println("Sweeny was here");
     }
+
+
 
 }
